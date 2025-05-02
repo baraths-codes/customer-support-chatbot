d0c1286a2a5a0ff9cff37780c5b9722594e59ff5
Baraths-codes <barath.codes@gmail.com>
2025-05-02 20:47:11 +0530

    debug: uncomment packages

--- chatbot/app.py
+++ chatbot/app.py
@@ -12,9 +12,9 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 # NLTK resources (make sure downloaded at least once)
-# nltk.download('punkt')
-# nltk.download('stopwords')
-# nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 # Load intents
 with open('./data/intents.json') as file:
